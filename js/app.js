4e0e82421678d615f685e8ff0d97e69dc6e80dce
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2021-09-02 17:59:25 +0600

    handle errors

--- js/app.js
+++ js/app.js
@@ -1,49 +1,78 @@
-const booksContainer = document.getElementById('display-books');
 const totalBooks = document.getElementById('total-books');
+const booksContainer = document.getElementById('display-books');
 
-// fetch books data from url 
+// handle fetch books data from url 
 const loadBooksData = () => {
     const searchField = document.getElementById('search-input');
     const searchValue = searchField.value;
     searchField.value = '';
+    totalBooks.textContent = '';
 
-    fetch(`http://openlibrary.org/search.json?q=${searchValue}`)
-        .then(res => res.json())
-        .then(data => displayBooks(data));
+    // loading spinner added before fetch the data
+    booksContainer.textContent = '';
+    totalBooks.textContent = '';
+    totalBooks.innerHTML = `
+    <div class="text-center mx-auto my-5">
+        <div class="spinner-border text-warning" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+    `;
+
+    // check the input value 
+    if (searchValue === '') {
+        booksContainer.innerHTML = '';
+        totalBooks.innerHTML = '';
+        totalBooks.innerHTML = `
+        <div class="alert-position alert alert-warning alert-dismissible fade show w-75 mx-auto  mt-5" role="alert">
+            <h3 class="text-danger">Please Enter a Value First!</h3>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+       </div>
+            `;
+
+    }
+    else {
+        fetch(`https://openlibrary.org/search.json?q=${searchValue}`)
+            .then(res => res.json())
+            .then(data => displayBooks(data));
+    }
+
+
+
+
 }
 
 // display books data
-const displayBooks = (booksData) => {
-    console.log(booksData);
+const displayBooks = booksData => {
+    // console.log(booksData);
     const { docs, numFound } = booksData;
-    console.log(docs);
-    console.log(numFound);
-    const totalDiv = document.createElement('div');
-    totalDiv.innerHTML = `
+    const totalBooks = document.getElementById('total-books');
+    totalBooks.textContent = '';
+    totalBooks.innerHTML = `
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         <h5 class="text-center">Found total ${numFound} books.</h5>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     `;
-    totalBooks.appendChild(totalDiv);
+
     const requiredBooks = docs.slice(0, 8);
     console.log(requiredBooks);
-
+    const booksContainer = document.getElementById('display-books');
+    booksContainer.textContent = '';
     requiredBooks.forEach(book => {
         console.log(book);
-        //const { cover_i, title, author_name, publisher, publish_date } = book;
         const booksDiv = document.createElement('div');
         booksDiv.classList.add('col');
         booksDiv.innerHTML = `
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Book Name: ${book?.title}</h5>
-                <p><span class="text-dark">Author Name:</span> ${book?.author_name}</p>
-                <P>Publisher: ${book?.publisher}</P>
-                <p>First Published date: ${book?.publish_date}</p>
-            </div>
+    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="Book Cover Image">
+    <div class="card-body">
+        <h5 class="card-title fw-bold">Book Name: ${book?.title ?? ''}</h5>
+        <p><span class="fw-bold">Author Name:</span> ${book?.author_name ?? ''}</p>
+         <P><span class="fw-bold">Publishers:</span> ${book?.publisher ?? ''}</P>
+        <p><span class="fw-bold">First Published:</span> ${book?.first_publish_year ?? ''}</p>
         </div>
-        `;
+    </div>
+    `;
         booksContainer.appendChild(booksDiv);
 
     });
